819079e709ce192a8226a68b2d6d86c06e3a6ba7
Peter Lesslie <pclesslie@gmail.com>
2014-03-26 14:39:55 -0500

    Add CMakeList.txt

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Config.h"
+
+using namespace std;
+
+int main( int argc, char **argv ) {
+  cout << "qsolve " << _VERSION_MAJOR << "." << _VERSION_MINOR << endl;
+  return 0;
+}
