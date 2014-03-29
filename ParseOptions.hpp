12268d4f1b96f3e20f0b3094cad0e5cd0b713709
Peter Lesslie <pclesslie@gmail.com>
2014-03-29 00:43:09 -0500

    Cleanup main
    Moved licensing functions out to Config.h

---
+++ ParseOptions.hpp
@@ -0,0 +1,8 @@
+#ifndef _PARSE_OPTIONS_HPP_
+#define _PARSE_OPTIONS_HPP_
+
+void print_version( char * program_name );
+void print_license();
+void print_boost_version();
+bool parse_
+#endif
--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
-#include <cstring>
 #include <string>
 #include "Config.h"
 #include "Database.hpp"
 #include "OrderEngine.hpp"
-#include "boost/version.hpp"
 #include "boost/program_options.hpp"
 
 using namespace std;
-
-void print_version( char * program_name );
-void print_license();
-void print_boost_version();
 
 int main( int argc, char **argv ) {
   boost::program_options::options_description desc( "Allowed options" );
@@ -64,23 +58,4 @@ int main( int argc, char **argv ) {
   return 0;
 }
 
-void print_version( char * program_name ) {
-  cout << program_name << " (QSolve) " << _VERSION_MAJOR << "." << _VERSION_MINOR << endl;
-  print_license();
-  cout << "\nWritten by " << _AUTHORS << endl;
-}
 
-void print_boost_version() {
-  cout << "Using Boost libs version: " << BOOST_VERSION / 100000 << "."
-       << BOOST_VERSION / 100 % 1000 << "."
-       << BOOST_VERSION % 100 << endl;
-}
-
-void print_license() {
-  cout << "Copyright (C) 2014 Grimberg, Hale, Polsley, Lesslie."
-       << endl
-       << "This is free software: you are free to change and redistribute it."
-       << endl
-       << "There is NO WARRANTY, to the extent permitted by law."
-       << endl;
-}
