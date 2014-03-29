233c3f1e6ae8fe48fe4aaeb2d22c3faeb5eaad57
Peter Lesslie <pclesslie@gmail.com>
2014-03-29 01:42:00 -0500

    Add simple command line parsing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include "QTypes.hpp"
 #include "Config.h"
 #include "Database.hpp"
 #include "OrderEngine.hpp"
@@ -8,54 +10,140 @@
 using namespace std;
 
 int main( int argc, char **argv ) {
-  boost::program_options::options_description desc( "Allowed options" );
-  desc.add_options()
+  //
+  // Use Boost.Program_Options to parse the command line and configuration file input
+  // Documentation for this library available at: http://www.boost.org/doc/libs/1_55_0/doc/html/program_options.html
+  //
+
+  string config_file;
+  string startDate;
+  string endDate;
+  string strategyFile;
+  capital initialBalance;
+
+
+  //
+  // Generic Options are the options that can only be specified on the command line
+  //
+  boost::program_options::options_description generic( "Generic options" );
+  generic.add_options()
     ( "help,h", "produce help message" )
-    ( "version,v", "print version" )
-    ( "start,s", boost::program_options::value<string>(), "start date for simulation" )
-    ( "end,e", boost::program_options::value<string>(), "end date for simulation" )
-    ( "strategy,S", boost::program_options::value<string>(), "python strategy file" )
+    ( "version", "print version" )
+    ( "config,c", boost::program_options::value<string>(&config_file)->default_value("qsolve.cfg"), "name of a configuration file")
     ;
 
-  boost::program_options::variables_map vm;
-  boost::program_options::store( boost::program_options::parse_command_line(argc, argv, desc), vm );
-  boost::program_options::notify( vm );
+  //
+  // Configuration options can be specified on the command line or in the configuration file
+  // The default configuration file name is "qsolve.cfg"
+  //
+  boost::program_options::options_description config( "Configuration options" );
+  config.add_options()
+    ( "start,s", boost::program_options::value<string>(&startDate)->required(), "start date for simulation" )
+    ( "end,e", boost::program_options::value<string>(&endDate)->default_value("today"), "end date for simulation" )
+    ( "balance,b", boost::program_options::value<capital>(&initialBalance)->default_value(0)->required(), "Initial amount of capital available" )
+    ;
 
-  if( vm.count( "help" ) ) {
-    cout << desc << endl;
+  //
+  // Hidden options can be specified on the command line or in the configuration file, but
+  // they will not be displayed in the help message because they are positional variables.
+  //
+  boost::program_options::options_description hidden( "Hidden Options" );
+  hidden.add_options()
+    ( "strategy", boost::program_options::value<string>(&strategyFile), "python strategy file" )
+    ;
+
+  //
+  // cmdline_options: options searched for on the command line
+  //
+  boost::program_options::options_description cmdline_options;
+  cmdline_options.add(generic).add(config).add(hidden);
+
+  //
+  // program_options: options searched for in the configuration file
+  //
+  boost::program_options::options_description config_file_options;
+  config_file_options.add(config).add(hidden);
+  
+  //
+  // visible: options that will be displayed in the help/usage message
+  //
+  boost::program_options::options_description visible("Allowed options");
+  visible.add(generic).add(config);
+
+  //
+  // positional arguments are ones that don't have to have a specifier before them.
+  // these arguments can be identified by where they are in the command line or
+  // the configuration file.
+  //
+  // The order of the specification on the command line determines where they are assigned.
+  // The order searched for is the the order in which they are add()'d to p
+  //
+  boost::program_options::positional_options_description p;
+  p.add("strategy", 1);
+  
+  
+  //
+  // variables_map is a type that maps the parsed command line options to their name as specified above
+  // store( command_line_parser( ... ) ... ) parses the command line arguments
+  // store( parse_config_file( ... ) ... ) parses the configuration file if there is one
+  //
+  try {
+    boost::program_options::variables_map vm;
+    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).
+				  options(cmdline_options).positional(p).run(), vm);
+    boost::program_options::notify( vm );
+    
+    ifstream ifs(config_file.c_str());
+    if(ifs) {
+      boost::program_options::store(boost::program_options::parse_config_file(ifs,config_file_options), vm);
+      boost::program_options::notify(vm);
+    }
+    
+    if( vm.count( "help" ) ) {
+      cout << visible << endl;
+      return 0;
+    }
+    
+    if( vm.count( "version" ) ) {
+      print_version( argv[0] );
+      return 0;
+    }
+    
+    if( vm.count( "start" ) ) {
+      cout << "Start date set to "
+	   << vm["start"].as<string>()
+	   << endl;
+    } else {
+      cout << "Must provide a start date for simulation." << endl;
+    }
+    
+    if( vm.count( "end" ) ) {
+      cout << "End date set to "
+	   << vm["end"].as<string>()
+	   << endl;
+    } else {
+      cout << "No end date given.  Using current date." << endl;
+    }
+    
+    if( vm.count( "strategy" ) ) {
+      cout << "Strategy file set to "
+	   << vm["strategy"].as<string>()
+	   << endl;
+    } else {
+      cout << "Must provide a strategy file." << endl;
+    }
+
+    //
+    // We have the command line arguments, now run the program
+    //
+
+    return 0;
+  } catch( const exception& e ) {
+    cout << e.what() << endl;
+    cout << endl;
+    cout << visible << endl;
     return 0;
   }
-
-  if( vm.count( "version" ) ) {
-    print_version( argv[0] );
-    return 0;
-  }
-
-  if( vm.count( "start" ) ) {
-    cout << "Start date set to "
-	 << vm["start"].as<string>()
-	 << endl;
-  } else {
-    cout << "Must provide a start date for simulation." << endl;
-  }
-
-  if( vm.count( "end" ) ) {
-    cout << "End date set to "
-	 << vm["end"].as<string>()
-	 << endl;
-  } else {
-    cout << "No end date given.  Using current date." << endl;
-  }
-
-  if( vm.count( "strategy" ) ) {
-    cout << "Strategy file set to "
-	 << vm["strategy"].as<string>()
-	 << endl;
-  } else {
-    cout << "Must provide a strategy file." << endl;
-  }
-
-  return 0;
 }
 
 
