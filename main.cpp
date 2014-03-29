a0907a6be67f80350fa37632feae2cd21054acac
Peter Lesslie <pclesslie@gmail.com>
2014-03-29 01:46:55 -0500

    Commenting main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main( int argc, char **argv ) {
   //
-  // Use Boost.Program_Options to parse the command line and configuration file input
+  // Use Boost.Program_Options to parse the command line and configuration file input.
   // Documentation for this library available at: http://www.boost.org/doc/libs/1_55_0/doc/html/program_options.html
   //
 
@@ -23,7 +23,7 @@ int main( int argc, char **argv ) {
 
 
   //
-  // Generic Options are the options that can only be specified on the command line
+  // Generic Options are the options that can only be specified on the command line.
   //
   boost::program_options::options_description generic( "Generic options" );
   generic.add_options()
@@ -33,8 +33,8 @@ int main( int argc, char **argv ) {
     ;
 
   //
-  // Configuration options can be specified on the command line or in the configuration file
-  // The default configuration file name is "qsolve.cfg"
+  // Configuration options can be specified on the command line or in the configuration file.
+  // The default configuration file name is "qsolve.cfg".
   //
   boost::program_options::options_description config( "Configuration options" );
   config.add_options()
@@ -53,19 +53,19 @@ int main( int argc, char **argv ) {
     ;
 
   //
-  // cmdline_options: options searched for on the command line
+  // cmdline_options: options searched for on the command line.
   //
   boost::program_options::options_description cmdline_options;
   cmdline_options.add(generic).add(config).add(hidden);
 
   //
-  // program_options: options searched for in the configuration file
+  // program_options: options searched for in the configuration file.
   //
   boost::program_options::options_description config_file_options;
   config_file_options.add(config).add(hidden);
   
   //
-  // visible: options that will be displayed in the help/usage message
+  // visible: options that will be displayed in the help/usage message.
   //
   boost::program_options::options_description visible("Allowed options");
   visible.add(generic).add(config);
@@ -76,29 +76,42 @@ int main( int argc, char **argv ) {
   // the configuration file.
   //
   // The order of the specification on the command line determines where they are assigned.
-  // The order searched for is the the order in which they are add()'d to p
+  // The order searched for is the the order in which they are add()'d to p.
   //
   boost::program_options::positional_options_description p;
   p.add("strategy", 1);
   
   
-  //
-  // variables_map is a type that maps the parsed command line options to their name as specified above
-  // store( command_line_parser( ... ) ... ) parses the command line arguments
-  // store( parse_config_file( ... ) ... ) parses the configuration file if there is one
-  //
   try {
+    //
+    // variables_map is a type that maps the parsed command line options to their name as specified above.
+    // store( command_line_parser( ... ) ... ) parses the command line arguments.
+    // store( parse_config_file( ... ) ... ) parses the configuration file if there is one.
+    //
     boost::program_options::variables_map vm;
+
+    //
+    // If any of the options are not recognized, or any of the required() arguments are not provided
+    // then store() will throw an exception.
+    //
     boost::program_options::store(boost::program_options::command_line_parser(argc, argv).
 				  options(cmdline_options).positional(p).run(), vm);
     boost::program_options::notify( vm );
     
+    //
+    // Try to open the configuration file to parse it.
+    // 
     ifstream ifs(config_file.c_str());
     if(ifs) {
       boost::program_options::store(boost::program_options::parse_config_file(ifs,config_file_options), vm);
       boost::program_options::notify(vm);
     }
     
+
+    //
+    // Now all parsed values should be in the variables_map vm.
+    // Can check if a variable is set with vm.count( VAR_NAME ) > 0.
+    //
     if( vm.count( "help" ) ) {
       cout << visible << endl;
       return 0;
@@ -134,11 +147,15 @@ int main( int argc, char **argv ) {
     }
 
     //
-    // We have the command line arguments, now run the program
+    // We have the command line arguments, now run the program.
     //
 
     return 0;
   } catch( const exception& e ) {
+    //
+    // Most likely store() threw an exception meaning that an option
+    // was not recognized, or one of the required options was not given.
+    //
     cout << e.what() << endl;
     cout << endl;
     cout << visible << endl;
